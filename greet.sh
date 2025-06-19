da8511ccea10244f606eecbaa7ff36ec6a1c38f5
Blessing Okoro <okoroblezyn25@gmail.com>
2025-06-19 00:26:26 +0000

    Practiced IF ststements

--- greet.sh
+++ greet.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/bash
 
-echo "Hello $name, welcome back!"
+NAME="Brad"
+# echo "My name is $NAME"
 
+#read -p "Enter your name: " NAME
+#echo "Hello $NAME, nice to meet you!"
+
+#if [ "$NAME" == "Blessing" ]
+#then
+#   echo "Your name is Blessing"
+#fi
+
+if [ "$NAME" == "Blessing" ]
+then
+   echo "Your name is Blessing"
+else
+   echo "Your name is not Blessing"
+fi
